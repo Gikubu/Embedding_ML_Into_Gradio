10b51c9e1fc1e5e034bfd33a40b07e6c42b0c19e
Gikubu <gikubujoseph@gmail.com>
2023-06-26 15:36:37 +0300

    functions to predict

--- gradio_project/assets/gr_app.py
+++ gradio_project/assets/gr_app.py
@@ -26,3 +26,31 @@ loaded_toolkit = load_ml_toolkit()
 encoder = loaded_toolkit["encoder"]
 scaler = loaded_toolkit["scaler"]
 model = loaded_toolkit["model"]
+
+
+# Function to predict
+def predict(*args, encoder = encoder, scaler = scaler, model = model):
+    input_data = pd.DataFrame([args], columns= expected_inputs)
+    input_data.fillna(0, inplace= True)
+    
+    encoder = loaded_toolkit["encoder"]
+    scaler = loaded_toolkit["scaler"]
+    model = loaded_toolkit["model"]
+
+    # Encoding the categorical columns
+    encoded_categoricals = encoder.transform(input_data[categoricals])
+    encoded_categoricals = pd.DataFrame(encoded_categoricals, columns= encoder.get_feature_names_out().tolist())
+    
+    # Adding the categorical columns to the input dataframe
+    df_encoded = input_data.join(encoded_categoricals)
+    df_encoded.drop(columns= categoricals, inplace=True)
+    
+    # Scaling the numeric columns
+    df_encoded[columns_to_scale] = scaler.transform(df_encoded[columns_to_scale])
+    
+    # Restricting column names to alpha-numeric characters
+    df_processed = df_encoded.rename(columns= lambda x: re.sub("[^A-Za-z0-9_]+", "", x))
+    
+    # Making the prediction
+    model_output = model.predict(df_processed)
+    return float(model_output[0])

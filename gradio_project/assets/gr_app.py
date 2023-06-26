3b1cc0bf2f25a38ac44bf786223f582c87998061
Gikubu <gikubujoseph@gmail.com>
2023-06-26 15:33:31 +0300

    functions

--- gradio_project/assets/gr_app.py
+++ gradio_project/assets/gr_app.py
@@ -11,3 +11,11 @@ expected_inputs = ["gender", "SeniorCitizen", "Partner", "Dependents", "Contract
 categoricals = ["gender", "SeniorCitizen", "Partner", "Dependents", "PhoneService", "MultipleLines", "InternetService", "OnlineSecurity",
                 "OnlineBackup", "DeviceProtection", "TechSupport", "StreamingTV", "StreamingMovies", "Contract", "PaperlessBilling", "PaymentMethod"]
 columns_to_scale = ["tenure", "MonthlyCharges", "TotalCharges"]
+
+
+# ----- Helper Functions and Imports
+# Function to load ML toolkit
+def load_ml_toolkit(filepath=r"C:\Users\asus\Data_Analytics\linear_regression_model.pkl"):
+    with open(filepath, "rb") as file:
+        loaded_toolkit = pickle.load(file)
+    return loaded_toolkit
